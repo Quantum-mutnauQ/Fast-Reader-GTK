9088e08bf482a9df27e03efd0eaf9b44052339ba
Quantum-mutnauQ <141065355+Quantum-mutnauQ@users.noreply.github.com>
2024-12-18 20:03:34 +0100

    Update FastReader.cpp

--- src/FastReader.cpp
+++ src/FastReader.cpp
@@ -91,11 +91,10 @@ static void save_settings() {
   config_setting_t *TimeToNextWord_setting = config_setting_add(root, "time_based_next_word", CONFIG_TYPE_BOOL);
   config_setting_set_bool(TimeToNextWord_setting, TimeToNextWord);
 
-  //double time_based_next_word_time = gtk_spin_button_get_value(global_TimeToNextWordSpinn);
-	  float time_based_next_word_time = 10;
+  double time_based_next_word_time = gtk_spin_button_get_value(global_TimeToNextWordSpinn);
+config_setting_t *time_based_next_word_time_setting = config_setting_add(root, "time_based_next_word_time", CONFIG_TYPE_FLOAT);
+config_setting_set_float(time_based_next_word_time_setting, time_based_next_word_time);
 
-  config_setting_t *time_based_next_word_time_setting = config_setting_add(root, "time_based_next_word_time", CONFIG_TYPE_FLOAT);
-  config_setting_set_float(time_based_next_word_time_setting, time_based_next_word_time);
     
   gboolean make_statistics = gtk_switch_get_active(global_StatisticsSwitch);
   config_setting_t *make_statistics_setting = config_setting_add(root, "make_statistics", CONFIG_TYPE_BOOL);
